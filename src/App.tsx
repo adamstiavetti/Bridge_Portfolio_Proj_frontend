b69b9d409c196c114b7195fedc953e209a7d4cd6
adamstiavetti <adamstiavetti@gmail.com>
2025-04-22 09:54:59 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <main className="p-6 max-w-xl mx-auto">
+      <h1 className="text-3xl font-bold text-blue-600">PlaceHolder</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
